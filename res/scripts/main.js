03a0331ad1da870c87cf4e008afab5bdda13ef6a
Dalaven Uind <dalaven1996@yahoo.com>
2021-09-16 21:57:33 -0500

    add icon, responsive

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -2,6 +2,8 @@ require([
 	"app",
 	"router"
 ], (app, router) => {
+	console.log(router.currentRoute.value);
 
 	app.use(router).mount("#app");
+
 });
--- res/scripts/router.js
+++ res/scripts/router.js
@@ -9,9 +9,13 @@ define([
 	const router = VueRouter.createRouter({
 		history: VueRouter.createWebHashHistory(),
 		routes: [
-			{ name: "index", path: "/", component: m("index") },
-			{ name: "home", path: "/home", component: m("home/index") },
+			{ name: "index", path: "/", component: m("index"), meta: { title: '' } },
+			{ name: "home", path: "/home", component: m("home/index"), meta: { title: 'Home' } },
 		]
+	});
+
+	router.afterEach((to, from) => {
+		document.title = to.meta.title + " | CHIKARA";
 	});
 
 	return router;
